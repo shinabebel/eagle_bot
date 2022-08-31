b7e9286dbca277443cf57838671e90c0c98e16eb
hoba_razor <shinabebel@gmail.com>
2022-08-31 22:37:20 +0800

    update

--- pages/api/item/thumbnail/[id].ts
+++ pages/api/item/thumbnail/[id].ts
@@ -1,4 +1,4 @@
-import { createReadStream } from 'fs';
+import { createReadStream, existsSync } from 'fs';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
@@ -12,15 +12,15 @@ export default async function handler(
   const path = decodeURI(result.data);
   const ext = path.substring(path.length - 3);
   if (result.status === 'success') {
-    try {
+    if (existsSync(path)) {
       const file = createReadStream(path);
       res.writeHead(200, {
         'Content-Type': `image/${ext}`,
-        'Content-Disposition': `attachment; filename="image.${ext}"`,
+        'Content-Disposition': `attachment; filename="thumbnail.${ext}"`,
       });
       file.pipe(res);
-    } catch (err) {
-      res.status(500).json(err);
+    } else {
+      res.status(500).send('File is not exist.');
     }
   } else {
     res.status(500).json(result.data);
--- pages/api/item/update.ts
+++ pages/api/item/update.ts
@@ -1,19 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<any>
+  req: NextApiRequest,
+  res: NextApiResponse<any>
 ) {
-    if (req.method === 'POST') {
-        const url = `${process.env.EAGLE_ENDPOINT}/item/update`;
-        const result = await fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(req.body),
-        }).then((response) => response.json());
-        console.log(result);
-        res.status(200).json(result.data);
-    } else {
-        res.status(500).json('GET method is not available.');
-    }
+  if (req.method === 'POST') {
+    const url = `${process.env.EAGLE_ENDPOINT}/item/update`;
+    const result = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(req.body),
+    }).then((response) => response.json());
+    console.log(result);
+    res.status(200).json(result.data);
+  } else {
+    res.status(500).json('GET method is not available.');
+  }
 }
